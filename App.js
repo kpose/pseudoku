ae731ea6a5cfaf100195f63b3e9a637c87b7f562
kpose <ganihujude@gmail.com>
2020-05-19 09:56:41 +0100

    conditionally render control menu

--- App.js
+++ App.js
@@ -23,6 +23,59 @@ export default class App extends React.Component {
     global.tilePress = global.tilePress.bind(this); 
   }
   render() {
+
+    let controlMenu = null;
+    
+    if (this.state.controlMenuVisible) {
+      controlMenu = (
+        <View style = {{ 
+              padding : 20, position: "absolute", zIndex: 9999,
+              flex: 1, alignItems: "stretch", justifyContent: "center",
+              borderRadius: 20, backgroundColor: "rgba(100, 64,255,0.95)",
+              width: this.state.controlMenuWidth, height: this.state.controlMenuHeight,
+              left: (this.state.screenUsableHeight - this.state.controlMenuHeight) / 2 }}>
+
+               <View style = {{ alignSelf: "center", paddingBottom: 40}}>
+                  <Text style = {{color:"#ffffff", fontSize: 24, fontWeight: "bold"}}>
+                    Control Menu
+                  </Text>
+               </View> 
+
+               <View style= {{ paddingBottom: 40, alignSelf: "center"}}>
+                 <Button title = "Start A New Game" style = {{ width: 150 }}
+                          onPress= {() => {
+                            state.numberOfTilesAcross = this.state.numberOfTilesAcross;
+                            state.numberOfTilesDown = this.state.numberOfTilesDown;
+                            this.setState(state, buildMatrix);
+                          }}
+                          />
+               </View>
+
+               <Text style = {{ color : "#ffffff" }}>Tiles Across</Text>
+               <Slider minimumValue = {3} maximumValue = {6} 
+                       value= {3} step = {1} maximumTrackTintColor= "white" 
+                       onSlidingComplete = { (value) => global.alterMatrixSize("across", value) }
+                        />
+                <Text style = {{ color: "#ffffff", paddingTop: 40}}>Tiles Down</Text>
+                <Slider minimumValue = {3} maximumValue = {6} 
+                       value= {3} step = {1} maximumTrackTintColor= "white" 
+                       onSlidingComplete = { (value) => global.alterMatrixSize("down", value) }
+                        />
+
+                <View style = {{ paddingTop : 40}}><Text style = {{color: "#ffffff"}}>
+                  Warning: Changing the grid size will automatically begin a new game!
+                </Text></View>
+
+                <View style = {{ paddingTop: 40, alignSelf: "center" }}>
+                  <Button title= "Done" style = {{ width: 150 }}
+                          onPress= { () => this.setState({
+                            controlMenuVisible: false, controlMenuButtonDisabled : false 
+                          }) }
+                          />
+                </View>
+              </View>
+      );
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
